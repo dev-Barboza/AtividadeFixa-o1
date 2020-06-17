6a1147d0dab55091e4cb5b6b3dcc1b649d59de38
Matheus <matheus.barboza.henrique@gmail.com>
2020-06-17 15:21:30 -0300

    projeto final

---
+++ Documents/SENAI/Cinemas/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Cinemas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           IngressoVip bilhete = new IngressoVip();
+           System.Console.WriteLine("Ola! Bem vindo Ao cinema !");
+           bilhete.ValorIngresso = 22f;
+           bilhete.ValorVip = 12f;
+           bilhete.ImprimirValor();
+           bilhete.AdicionarValor();
+
+           System.Console.WriteLine("A diferença de valor é R$ " + bilhete.ValorVip);
+          
+           
+           
+           
+    }
+ }
+}
